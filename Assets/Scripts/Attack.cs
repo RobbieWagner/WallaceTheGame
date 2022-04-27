5816a604a332a16dbe863c7dac1d5536aff56d64
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-04-26 20:07:37 -0500

    Pulling my assets from class project

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // How frequently can we attack?
+     public float attackTimeLimit = 0.5f;
+
+     // Countdown timer for attacks
+     private float attackCountdownTimer = 0;
+
+     // An empty parented that says where to attack
+     public Transform attackPos;
+     // Radius of attack circle
+     public float attackRange;
+     // What layer will the enemies be on?
+     public LayerMask enemyLayer;
+     // How much damage to deal
+     public int damage = 3;
+
+     public BoxCollider2D bc2d;
+     public float boxCastDistance;
+     bool canMove;
+
+     void Update()
+     {
+
+         canMove = gameObject.GetComponent<Character>().canMove;
+
+         // See if we can attack, via timer.
+         if (attackCountdownTimer <= 0)
+         {
+             // We can attack. See if user hit space bar.
+             if (Input.GetKey(KeyCode.J))
+             {
+                 // Reset the countdown timer
+                 attackCountdownTimer = attackTimeLimit;
+                 // What enemies did we hit?
+                RaycastHit2D hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, boxCastDistance, enemyLayer);
+
+                if (!hit)
+                {
+                    hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.up, boxCastDistance, enemyLayer);
+                }
+                if (!hit)
+                {
+                    hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.right, boxCastDistance, enemyLayer);
+                }
+                if (!hit)
+                {
+                    hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.left, boxCastDistance, enemyLayer);
+                }
+                if (hit && canMove)
+                {
+                 // Loop through each enemy we hit
+                     // Get the enemy script attached to this object
+                     PlantedPumpkin enemyScript = hit.transform.gameObject.GetComponent<PlantedPumpkin>();
+                     // If there is an enemy script
+                     if (enemyScript)
+                     {
+                         // Damage
+                         if(enemyScript.health > 1) StartCoroutine(enemyScript.ShakePumpkin());
+                         enemyScript.health -= damage;
+                     }
+                 }
+             }
+         }
+         else
+         {
+             // Attack timer needs count-down
+             attackCountdownTimer -= Time.deltaTime;
+         }
+     }
+     // Used to draw a circle when we are selecting the player in the scene view
+     void OnDrawGizmosSelected()
+     {
+         Gizmos.color = Color.red;
+         Gizmos.DrawWireSphere(attackPos.position, attackRange);
+     }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,268 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Character : MonoBehaviour
+{
+    // Code adapted from MyCharacterController (Dr.Craven)
+
+    public int score = 0;
+
+    Rigidbody2D body;
+
+    BoxCollider2D bc2d; // Handles boxCasting, which tells if the player interacts with things
+    public LayerMask interactable;
+    public float boxCastDistance;
+
+    public bool canMove;
+    bool hasDied; // Keeps track of whether or not the player has any deaths
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    public float runSpeed = 5.0f;
+    public Animator animator;
+
+    public Animator ghostAnimator;
+    private bool ghostSeen;
+    public Transform ghost;
+    public GameObject ghostParticles;
+    
+    public GameObject scoreGO;
+    public Text scoreText;
+
+    public Text dialogueText;
+    public string openingDialoguePath;
+    public string allergyDialoguePath;
+
+    string dialoguePath;
+    AudioSource dialogueSound;
+
+    public AudioSource pickupSound;
+    public AudioSource goalSound;
+
+    float characterOriginX;
+    float characterOriginY;
+
+    public float boxCastSize;
+
+    void Start()
+    {
+        // Get the rigid body component for the player character.
+        // (required to have one)
+        body = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+
+        characterOriginX = transform.position.x;
+        characterOriginY = transform.position.y;
+
+        ghostSeen = false;
+        hasDied = false;
+
+        scoreText.fontSize = 20;
+
+        StartCoroutine(ReadDialogue(new StreamReader(openingDialoguePath)));
+    }
+
+    void Update()
+    {
+        if(canMove)
+        {
+            horizontal = Input.GetAxisRaw("Horizontal"); 
+            vertical = Input.GetAxisRaw("Vertical"); 
+
+            animator.SetFloat("Speed X", horizontal);
+            animator.SetFloat("Speed Y", vertical);
+
+            if(gameObject.transform.position.y > 35.5f && gameObject.transform.position.x > 32f)
+            {
+                ghostAnimator.SetBool("FloatingAway", true);
+                ghostSeen = true;
+                ghostParticles.SetActive(true);
+            }
+
+            if(ghostSeen && ghost.position.y > -50)
+            {
+                ghost.Translate(-.0275f, 0, 0);
+            }
+            else if(ghostSeen)
+            {
+                Destroy(ghost.gameObject);
+            }
+        }
+
+        if(Input.GetKeyDown("space"))
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, boxCastDistance, interactable);
+
+            if (!hit)
+            {
+                hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.up, boxCastDistance, interactable);
+            }
+            if (!hit)
+            {
+                hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.right, boxCastDistance, interactable);
+            }
+            if (!hit)
+            {
+                hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.left, boxCastDistance, interactable);
+            }
+            if (hit && canMove)
+            {
+                //Completes any special actions from NPCs
+                GameObjectSwitch objectOnOffSwitch = hit.transform.gameObject.GetComponent<GameObjectSwitch>();
+                if(objectOnOffSwitch != null)
+                {
+                    objectOnOffSwitch.GameObjectOnOff();
+                }
+
+                DialogueHolder dialogueInfo = hit.transform.gameObject.GetComponent<DialogueHolder>();
+
+                GrandpasGift grandpasGift = hit.transform.gameObject.GetComponent<GrandpasGift>();
+                if(grandpasGift != null && !(dialogueInfo.interactedOnce))
+                {
+                    StartCoroutine(grandpasGift.GiftPlayer(this.gameObject.GetComponent<Character>()));
+                }
+
+                Percy_Movement percyMovement = hit.transform.gameObject.GetComponent<Percy_Movement>();
+                if(percyMovement != null)
+                {
+                    percyMovement.interacting = true;
+                    StartCoroutine(percyMovement.BeginWalkingAgain());
+                }
+
+                //Finds the correct dialogue info for the NPC
+                Color color = dialogueInfo.color;
+                string dialoguePath;
+                if(!dialogueInfo.interactedOnce)
+                {             
+                    dialoguePath = dialogueInfo.dialoguePath;
+                }
+                else
+                {
+                    dialoguePath = dialogueInfo.alternatePath;
+                }
+                dialogueSound = dialogueInfo.dialogueSound;
+                float lowPitch = dialogueInfo.lowPitch;
+                float highPitch = dialogueInfo.highPitch;
+                StartCoroutine(ReadDialogue(new StreamReader(dialoguePath), dialogueSound, color, lowPitch, highPitch, dialogueInfo.interactedOnce, dialogueInfo));
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        // If player is running diagonally, we don't want them to move extra-fast.
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        if(canMove){
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D colliderEvent)
+    {
+        
+        if(colliderEvent.gameObject.CompareTag("Enemy"))
+        {
+            // Respawn the player, they keep their points
+            body.velocity = new Vector2(0, 0);
+            transform.position = new Vector2(characterOriginX, characterOriginY);
+            animator.SetFloat("Speed X", 0);
+            animator.SetFloat("Speed Y", 0);
+
+            if(!hasDied)
+            {
+                hasDied = true;
+                StartCoroutine(ReadDialogue(new StreamReader(allergyDialoguePath)));
+            }
+        }
+    }
+
+    IEnumerator ReadDialogue(StreamReader dialogueReader)
+    {
+        //Stop the player from moving
+        body.velocity = new Vector2(0, 0);
+        animator.SetFloat("Speed X", 0);
+        animator.SetFloat("Speed Y", 0);
+        canMove = false;
+
+        string line;
+        scoreGO.SetActive(false);
+        dialogueText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.5f);
+
+        while((line = dialogueReader.ReadLine()) != " ")
+        {
+            dialogueText.text = line;
+            yield return new WaitForSeconds(.5f);
+            while((line = dialogueReader.ReadLine()) != "")
+            {
+                dialogueText.text = line;
+                yield return new WaitForSeconds(.5f);
+            }
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        //reverts all changes made, letting the player continue playing
+        dialogueText.text = "";
+        dialogueText.gameObject.SetActive(false);
+        canMove = true;
+        dialogueReader.Close();
+        StopCoroutine(ReadDialogue(dialogueReader));
+    }
+
+    IEnumerator ReadDialogue(StreamReader dialogueReader, AudioSource dialogueSound, Color color, float lowPitch, float highPitch, bool interactedOnce, DialogueHolder dialogueInfo)
+    {
+        //Stop the player from moving
+        body.velocity = new Vector2(0, 0);
+        animator.SetFloat("Speed X", 0);
+        animator.SetFloat("Speed Y", 0);
+        canMove = false;
+        string line;
+        scoreGO.SetActive(false);
+        dialogueText.gameObject.SetActive(true);
+        dialogueText.color = color;
+        while((line = dialogueReader.ReadLine()) != " ")
+        {
+            dialogueText.text = line;
+            yield return new WaitForSeconds(.5f);
+            while((line = dialogueReader.ReadLine()) != "")
+            {
+                dialogueText.text = line;
+                dialogueSound.pitch = Random.Range(lowPitch, highPitch);
+                dialogueSound.Play();
+                yield return new WaitForSeconds(.5f);
+            }
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        //reverts all changes made, letting the player continue playing
+        dialogueText.text = "";
+        dialogueText.gameObject.SetActive(false);
+        canMove = true;
+        dialogueReader.Close();
+        dialogueText.color = Color.white;
+        if(!dialogueInfo.interactedOnce) StartCoroutine(dialogueInfo.firstInteraction());
+        StopCoroutine(ReadDialogue(dialogueReader, dialogueSound, color, lowPitch, highPitch, interactedOnce, dialogueInfo));
+    }
+
+    public IEnumerator TurnOnScoreTemporarily()
+    {
+        scoreGO.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        scoreGO.gameObject.SetActive(false);
+        StopCoroutine(TurnOnScoreTemporarily());
+    }
+}
