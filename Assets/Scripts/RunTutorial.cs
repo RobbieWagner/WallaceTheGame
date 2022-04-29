5f87ee38bcd6101772a8e568f5386d62243503e9
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-04-29 08:12:07 -0500

    More Tutorial Added

--- Assets/Scripts/RunTutorial.cs
+++ Assets/Scripts/RunTutorial.cs
@@ -8,22 +8,38 @@ public class RunTutorial : MonoBehaviour
 
     public Character character;
     public string openingDialoguePath;
+    public string tutorialPart2Path;
+
+    public GameObject wasdControls;
+
+    bool hasMoved = false;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Tutorial());
+
+        hasMoved = false;
+        wasdControls.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(character.canMove &&(Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))) hasMoved = true;
     }
 
     IEnumerator Tutorial()
     {
         yield return StartCoroutine(character.ReadDialogue(new StreamReader(openingDialoguePath)));
+
+        wasdControls.SetActive(true);
+        while(!hasMoved) yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.5f);
+        wasdControls.SetActive(false);
+
+        yield return StartCoroutine(character.ReadDialogue(new StreamReader(tutorialPart2Path)));
+
         StopCoroutine(Tutorial());
     }
 }
