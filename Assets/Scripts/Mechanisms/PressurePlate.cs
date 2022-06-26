07a76a27ff986b878707014fdc8276e21819940a
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-06-26 11:04:18 -0500

    work on #25

--- Assets/Scripts/Mechanisms/PressurePlate.cs
+++ Assets/Scripts/Mechanisms/PressurePlate.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PressurePlate : MonoBehaviour
 {
+
+    //New Skill: To change the color of a tilemap, import tilemaps and get the tilemap component.
 
     public Animator buttonAnimator;
 
@@ -12,17 +15,22 @@ public class PressurePlate : MonoBehaviour
 
     public Transform objectT;
     public Collider2D objectC;
+    public Tilemap objectTilemap;
     public Vector2 initialPosition;
     public Vector2 finalPosition;
+
+    private bool objectRising;
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = objectT.position;
+        objectRising = false;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        objectRising = true;
         buttonAnimator.SetBool("ButtonPressed", true);
         objectToMove.bodyType = RigidbodyType2D.Dynamic;
         objectC.enabled = false;
@@ -32,16 +40,22 @@ public class PressurePlate : MonoBehaviour
     {
         if(objectT.position.x != finalPosition.x && objectT.position.y != finalPosition.y)
         {
-            objectT.position = Vector2.MoveTowards(objectT.position, finalPosition, .01f);
+            objectT.position = Vector2.MoveTowards(objectT.position, finalPosition, .1f);
         }
         else
         {
             objectToMove.bodyType = RigidbodyType2D.Static;
         }
+
+        if(!(objectTilemap.color.a <= .1f))
+        {
+            objectTilemap.color = new Vector4(objectTilemap.color.r, objectTilemap.color.g, objectTilemap.color.b, objectTilemap.color.a - .01f);
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
+        objectRising = false;
         buttonAnimator.SetBool("ButtonPressed", false);
         objectToMove.bodyType = RigidbodyType2D.Dynamic;
         StartCoroutine(resetMechanism());
@@ -49,10 +63,15 @@ public class PressurePlate : MonoBehaviour
 
     IEnumerator resetMechanism()
     {
-        while(objectT.position.x != initialPosition.x && objectT.position.y != initialPosition.y)
+        while(!(objectT.position.x <= initialPosition.x) && !(objectT.position.y <= initialPosition.y) && !objectRising)
         {
-            objectT.position = Vector2.MoveTowards(objectT.position, initialPosition, .01f);
+            Debug.Log("hi");
+            objectT.position = Vector2.MoveTowards(objectT.position, initialPosition, .1f);
             yield return new WaitForSeconds(.001f);
+            if(!(objectTilemap.color.a >= 1f))
+            {
+                objectTilemap.color = new Vector4(objectTilemap.color.r, objectTilemap.color.g, objectTilemap.color.b, objectTilemap.color.a + .01f);
+            }
         }
         objectC.enabled = true;
         objectToMove.bodyType = RigidbodyType2D.Static;
